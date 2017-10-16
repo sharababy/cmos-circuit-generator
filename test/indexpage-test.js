4c347c5725c55b2017b0e3a2c74228d1bc73a7b9
Vedant Bassi <bassi.vdt@gmail.com>
2017-10-16 17:01:20 +0530

    Fix Test

--- test/indexpage-test.js
+++ test/indexpage-test.js
@@ -1,6 +1,10 @@
 var expect  = require('chai').expect;
 var request = require('request');
 
+var server = require('../server');
+
+
+
 
 it('Should give Correct NAND Logic', function(done) {
     request('http://localhost:3001/=a1b' , function(error, response, body) {
@@ -14,5 +18,9 @@ it('Should give Correct NOR Logic', function(done) {
     request('http://localhost:3001/=a2b' , function(error, response, body) {
         expect(body).to.equal(`[{"name":"(a)","alias":"F","value":true,"count":0,"eval":0,"input1":null,"input2":null},{"name":"(b)","alias":"G","value":false,"count":0,"eval":0,"input1":null,"input2":null},{"name":"((b) ^ (a))","alias":"H","value":false,"input1":{"name":"(b)","alias":"G","value":false,"count":0,"eval":0,"input1":null,"input2":null},"input2":{"name":"(a)","alias":"F","value":true,"count":0,"eval":0,"input1":null,"input2":null},"count":1,"op":2,"eval":1}]`);
         done();
+        
     });
+
 });
+
+
